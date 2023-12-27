9ef117f615435eb58af5841afe9c761121cc04b0
Will Price <pricew86@gmail.com>
2023-12-27 11:42:52 +0800

    initial commit

---
+++ src/Footer.tsx
@@ -0,0 +1,13 @@
+export default function Footer() {
+  return (
+    <small>
+      <p>
+        &copy; Copyright by{' '}
+        <a target='_blank' href='/'>
+          Kinexis
+        </a>{' '}
+        2024{' '}
+      </p>
+    </small>
+  );
+}
