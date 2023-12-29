ecf9aa746de50c8bdf1b4cb68aa6dfbf473b3989
Will Price <pricew86@gmail.com>
2023-12-29 14:22:47 +0700

    style header

---
+++ src/components/Footer.tsx
@@ -0,0 +1,15 @@
+export default function Footer() {
+  return (
+    <footer className='footer'>
+      <small>
+        <p>
+          &copy; Copyright by{' '}
+          <a target='_blank' href='/'>
+            Kinexis
+          </a>{' '}
+          2024{' '}
+        </p>
+      </small>
+    </footer>
+  );
+}
